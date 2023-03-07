988e560669d16cd4044fbeaef789d532e0de93d4
alex01081901 <114284497+alex01081901@users.noreply.github.com>
2023-03-07 19:53:20 +0200

    fin

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,21 @@
-const listWithId = document.querySelectorAll('.item');
-console.log(listWithId);
-console.log("Hello, world!");
- 
-const totalItem = 0;
-listWithId.forEach(function (element) {
+const withIdEl = document.querySelectorAll('.item');
+console.log(withIdEl);
+const itemEl = document.querySelectorAll('ul');
+console.log(itemEl);
 
- totalItem += element});
+
+console.log(`Number of categories: ${withIdEl.length}`);
+
+withIdEl.forEach(value => {
+  
+   const firstWithIdEl = value.querySelector('h2'); 
+   const itemEl = value.querySelector('ul');
+
+   console.log(`Categories:${firstWithIdEl.textContent}`);
+ console.log(`Elements:${itemEl.children.length}`);
+
+
+});
+
+
+
