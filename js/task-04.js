bd77efaafeb7ba77afe1e9b924d659f2f8682b14
alex01081901 <114284497+alex01081901@users.noreply.github.com>
2023-03-11 19:28:41 +0200

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,18 @@
+const increment = document.querySelector('[data-action = "increment"]');
+const decrement = document.querySelector('[data-action = "decrement"]');
+const showValue = document.getElementById("value");
+
+let counterValue = 0;
+
+increment.addEventListener("click", btnIncrementHandler);
+decrement.addEventListener("click", btnDecrementHandler);
+
+function btnIncrementHandler(event) {
+    console.log((counterValue += 1));
+}
+
+function btnDecrementHandler(event) {
+    console.log((counterValue -= 1));
+    showValue.textContent = counterValue;
+
+}
